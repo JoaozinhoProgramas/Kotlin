c3ffaa0128573e75040a1a45452367110e63474f
JoaozinhoProgramas <joao90510245@edu.unipar.br>
2026-03-23 20:26:33 -0300

    Add files via upload

--- KOTLIN UILSON/Mercado/Main.kt
+++ KOTLIN UILSON/Mercado/Main.kt
@@ -1,10 +1,17 @@
 fun main() {
     
+    var NameList : ArrayDeque <String> = ArrayDeque()
+    var PriceList : ArrayDeque <Double> = ArrayDeque()
+    var IDList : ArrayDeque <Int> = ArrayDeque()
+    var QuantList : ArrayDeque <Int> = ArrayDeque()
+    
+    var Pid : Int = 0
+    
     do{
         println("Mercado")
         println("1 - Comprar")
         println("2 - Vender")
-        println("3 - Alterar Mercadoria")
+        println("3 - Mostrar estoque")
         println("4 - Encerrar aplicação")
         
         val Choice : Int = readln().toInt()
@@ -13,33 +20,78 @@ fun main() {
             when(Choice){
                 1 -> {
                     println("={COMPRAS}=")
-                    
-                    var Pid : Int = 0
-                    val product : cProduct = Product()//dar um jeito de instanciar
                         
                     println("Digite o nome do produto: (Digite S para sair)")
-                    product.PName = readln()
+                    var PName : String = readln()
                         
                     if (PName.uppercase() == "S") {                        
-                        println("Encerrando compra ...")
+                        println("Encerrando janela de compra ...")
                         break
                     }
                         
                     println("Digite a quantidade que irá comprar: ")
-                    product.PQuant = readln().toInt()
+                    var PQuant : Int = readln().toInt()
+                    
                         
                     println("Digite o valor do produto: ")
-                    product.PPrice = readln().toDouble()
+                    var PPrice : Double = readln().toDouble()
+                    
+                    println("=============")
+                    println("ID: ${Pid}")
+                    println("NOME: ${PName}")
+                    println("PREÇO:R$ ${PPrice}" )
+                    println("QUANTIDADE: ${PQuant} unid")
+                    println("=============")
+                   
+                    NameList.addLast(PName)
+                    QuantList.addLast(PQuant)
+                    PriceList.addLast(PPrice)
+                    IDList.addLast(Pid)
                     
                     Pid += 1
                 } 
                 2 -> println("Vendeu")
-                3 -> println("Compre algo antes")
+                3 -> {
+                    println("={ESTOQUE}=")
+                    var i : Int = 0
+                    
+                    for(y in 0 until NameList.size) {
+                    
+                        println("=============")
+                        println("ID: ${IDList[i]}")
+                        println("NOME: ${NameList[i]}")
+                        println("PREÇO:R$ ${PriceList[i]}" )
+                        println("QUANTIDADE: ${QuantList[i]} unid")
+                        println("=============")
+                        
+                        i += 1
+                    }
+                    println("Deseja alterar algum produto? S/N")
+                    var Change : String = readln().uppercase()
+                    
+                    if (Change == "N") {
+                        println("Encerrando janela do estoque ...")
+                        break
+                    }else if (Change == "S") {
+                        println("Qual produto será alterado?")
+                        for(y in 0 until NameList.size) {
+                            println("Produto ${NameList[i]}")
+                            i += 1
+                        }
+                    } else {
+                        println("Comando inválido, tente novamente")
+                    }
+                        
+                    
+                }
                 4 -> {
                     println("Encerrando aplicação...")
                 }
                     
-                else -> println("Comando inválido, tente novamente")
+                else -> {
+                    println("Comando inválido, tente novamente")
+                    break
+                }
             }
         }
 
